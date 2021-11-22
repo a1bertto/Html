37158ae94b4e9d106472cd49f6c719a43bd7febf
Albert Arthur <albertarthur53@gmail.com>
2021-11-22 17:03:08 +0000

    Angular folder added

---
+++ PracticeWork/index.ts
@@ -0,0 +1,20 @@
+// first thing to get axios object
+
+//import axios  from 'axios'
+
+
+
+//const URL = "https://jsonplaceholder.typicode.com/posts";
+
+
+
+console.log("Begin of data fetching...");
+
+// use axios api to send request to other web
+
+
+
+//let p = axios.get(URL);
+
+//console.log(p);
+
